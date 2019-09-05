d1a0db111f6f039eb4f656a876d128fe3e06d45d
EvilSourcerer <idontwantyourmessagesdont@mail.me>
2019-09-05 12:28:18 -0400

    add types for options

--- index.ts
+++ index.ts
@@ -2,107 +2,34 @@ import mc from 'minecraft-protocol';
 import { EventEmitter } from 'events';
 import pluginLoader from './lib/plugin_loader';
 import { supportedVersions } from './lib/version';
-const plugins = {
-  bed: require('./lib/plugins/bed'),
-  title: require('./lib/plugins/title'),
-  block_actions: require('./lib/plugins/block_actions'),
-  blocks: require('./lib/plugins/blocks'),
-  book: require('./lib/plugins/book'),
-  boss_bar: require('./lib/plugins/boss_bar'),
-  chat: require('./lib/plugins/chat'),
-  chest: require('./lib/plugins/chest'),
-  command_block: require('./lib/plugins/command_block'),
-  craft: require('./lib/plugins/craft'),
-  creative: require('./lib/plugins/creative'),
-  digging: require('./lib/plugins/digging'),
-  dispenser: require('./lib/plugins/dispenser'),
-  enchantment_table: require('./lib/plugins/enchantment_table'),
-  entities: require('./lib/plugins/entities'),
-  experience: require('./lib/plugins/experience'),
-  furnace: require('./lib/plugins/furnace'),
-  game: require('./lib/plugins/game'),
-  health: require('./lib/plugins/health'),
-  inventory: require('./lib/plugins/inventory'),
-  kick: require('./lib/plugins/kick'),
-  physics: require('./lib/plugins/physics'),
-  rain: require('./lib/plugins/rain'),
-  ray_trace: require('./lib/plugins/ray_trace'),
-  scoreboard: require('./lib/plugins/scoreboard'),
-  settings: require('./lib/plugins/settings'),
-  simple_inventory: require('./lib/plugins/simple_inventory'),
-  sound: require('./lib/plugins/sound'),
-  spawn_point: require('./lib/plugins/spawn_point'),
-  tablist: require('./lib/plugins/tablist'),
-  time: require('./lib/plugins/time'),
-  villager: require('./lib/plugins/villager')
+
+interface IPluginOptions {
+  [key: string]: boolean
 }
 
-export function createBot (options = {}) {
-  options.username = options.username || 'Player'
-  options.version = options.version || false
-  options.plugins = options.plugins || {}
-  options.logErrors = options.logErrors === undefined ? true : options.logErrors
-  options.loadInternalPlugins = options.loadInternalPlugins !== false
-  const bot = new Bot()
-  if (options.logErrors) {
-    bot.on('error', err => console.log(err))
-  }
+interface IOptions {
+  username?: string;
+  password?: string;
+  host?: string;
+  port?: number;
+  version?: string;
+  clientToken?: string;
+  accessToken?: string;
+  logErrors?: boolean;
+  keepAlive?: boolean;
+  checkTimeoutInterval: number;
+  loadInternalPlugins?: boolean;
+  plugins?: IPluginOptions;
+  chat?: 'enabled' | 'commandsOnly' | 'disabled';
+  colorsEnabled?: boolean;
+  viewDistance?: 'far' | 'normal' | 'short' | 'tiny';
+  difficulty?: string;
+  showCape?: boolean;
+  chatLengthLimit?: number;
+};
 
-  pluginLoader(bot, options)
-  const internalPlugins = Object.keys(plugins)
-    .filter(key => {
-      if (typeof options.plugins[key] === 'function') return
-      if (options.plugins[key] === false) return
-      return options.plugins[key] || options.loadInternalPlugins
-    }).map(key => plugins[key])
-  const externalPlugins = Object.keys(options.plugins)
-    .filter(key => {
-      return typeof options.plugins[key] === 'function'
-    }).map(key => options.plugins[key])
-  bot.loadPlugins([...internalPlugins, ...externalPlugins])
-
-  bot.connect(options)
-  return bot
-}
-
-class Bot extends EventEmitter {
-  constructor () {
-    super()
-    this._client = null
-  }
-
-  connect (options) {
-    const self = this
-    self._client = mc.createClient(options)
-    self.username = self._client.username
-    self._client.on('session', () => {
-      self.username = self._client.username
-    })
-    self._client.on('connect', () => {
-      self.emit('connect')
-    })
-    self._client.on('error', (err) => {
-      self.emit('error', err)
-    })
-    self._client.on('end', () => {
-      self.emit('end')
-    })
-    if (!self._client.wait_connect) next()
-    else self._client.once('connect_allowed', next)
-    function next () {
-      const version = require('minecraft-data')(self._client.version).version
-      if (supportedVersions.indexOf(version.majorVersion) === -1) {
-        throw new Error(`Version ${version.minecraftVersion} is not supported.`)
-      }
-      self.protocolVersion = version.version
-      self.majorVersion = version.majorVersion
-      self.version = version.minecraftVersion
-      options.version = version.minecraftVersion
-      self.emit('inject_allowed')
-    }
-  }
-
-  end () {
-    this._client.end()
+export class Bot extends EventEmitter {
+  constructor(options: IOptions) {
+    super();
   }
 }
